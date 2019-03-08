c94829bd18ccddfede0545af22644c035e50b446
nayshlok <borlandavid@gmail.com>
2019-03-08 11:38:27 -0700

    Commit actual build artifacts

---
+++ docs/precache-manifest.05e9ab5783cc4eb1865c88c6f0ef8157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a37c662c90f23f32b1d",
+    "url": "/static/css/main.f88357db.chunk.css"
+  },
+  {
+    "revision": "0a37c662c90f23f32b1d",
+    "url": "/static/js/main.8e6b77cc.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f18a9cbac1fd3778050f",
+    "url": "/static/js/2.7660927c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad323f950f44e0bb3028c02edc78edab",
+    "url": "/index.html"
+  }
+];
