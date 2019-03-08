8cb8fb1c784e839a71cdd58770ef3f9c42cb0c0b
nayshlok <borlandavid@gmail.com>
2019-03-08 12:15:37 -0700

    Updates

---
+++ precache-manifest.f83574d0ae6c336aa93505dc473ee9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b455596d3599f35c1ef5",
+    "url": "/automate-me/static/css/main.f88357db.chunk.css"
+  },
+  {
+    "revision": "b455596d3599f35c1ef5",
+    "url": "/automate-me/static/js/main.999795e3.chunk.js"
+  },
+  {
+    "revision": "32c71cc9de582a3a05f5",
+    "url": "/automate-me/static/js/runtime~main.e9e4e5bd.js"
+  },
+  {
+    "revision": "f18a9cbac1fd3778050f",
+    "url": "/automate-me/static/js/2.7660927c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/automate-me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ecd291546289dc933eab84cb62b791a",
+    "url": "/automate-me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05e9ab5783cc4eb1865c88c6f0ef8157.js"
+  "/automate-me/precache-manifest.f83574d0ae6c336aa93505dc473ee9a4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/automate-me/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
